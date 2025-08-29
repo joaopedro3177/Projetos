23886cbedbcc8506c807c53e10bcb8ccca0e2660
joaopedro3177 <joaopedrosimiao2008@gmail.com>
2025-08-29 16:03:01 -0300

    While

--- projetosPy/caixaEletronico.py
+++ projetosPy/caixaEletronico.py
@@ -3,16 +3,24 @@ resp = int(input("O que você gostaria de fazer? Digite: (1) para saque, (2) par
 
 if resp == 1 :
     valorSaque = int(input("Qual valor você gostaria de sacar?"))
-    if valorSaque <= saldo:
+    if valorSaque <= 0:
+        print("Não é posível sacar esse valor!")
+        
+    elif valorSaque <= saldo:
         saldo -= valorSaque
         print("Valor sacado: ", valorSaque)
         print("Seu saldo atual ficou com um valor de: ", saldo)
+
     else:
         print("Saldo insuficiente!")
 
 elif resp == 2:
     valorDeposito = int(input("Digite o valor a ser depositado:"))
-    if valorDeposito > 0:
-        
+    while valorDeposito <= 0:
+        print("Não é possível depositar ", valorDeposito, "Reais!")
+        valorDeposito = int(input("Digite o valor a ser depositado:"))
 
-
+    else:
+        saldo += valorDeposito
+        print("Valor de ", valorDeposito, "Reais depositado!")
+        print("Seu saldo atual ficou com um valor de: ", saldo)
