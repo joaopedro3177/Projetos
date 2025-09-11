d4542891a6f8d486bd0acbb7f3a50a953a3c5846
joaopedro3177 <joaopedrosimiao2008@gmail.com>
2025-09-11 16:26:40 -0300

    .

--- projetosPy/exercicios.py
+++ projetosPy/exercicios.py
@@ -1,10 +1,1 @@
-cont = 0
-asteristicos = "***************"
-
-while cont <= len(asteristicos):
-    contG = 0
-    while contG < cont:
-        print(asteristicos[contG])
-        contG += 1
-    cont += 1
 
