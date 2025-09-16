4557d319a1fb819c7b287c20843c5733a7bb7134
joaopedro3177 <joaopedrosimiao2008@gmail.com>
2025-09-16 16:39:20 -0300

    Finalizado, aguardando melhoras!

--- projetosPy/caixaEletronico.py
+++ projetosPy/caixaEletronico.py
@@ -1,12 +1,23 @@
+saldo = 0
+resp = 0
+saldo = 0
+valorSaque = 0
+valorDeposito = 0
+
+
+
 saldo = int(input("Digite seu saldo:"))
-resp = int(input("O que você gostaria de fazer? Digite: (1) para saque, (2) para deposito e (3) para vizualizar o saldo"))
+resp = int(input("O que você gostaria de fazer? Digite: (1) para saque, (2) para deposito, (3) para vizualizar o saldo e (4) para sair"))
+
+
 
 if resp == 1 :
     valorSaque = int(input("Qual valor você gostaria de sacar?"))
-    if valorSaque <= 0:
+    while valorSaque <= 0:
         print("Não é posível sacar esse valor!")
-        
-    elif valorSaque <= saldo:
+        valorSaque = int(input("Qual valor você gostaria de sacar?"))
+             
+    if valorSaque <= saldo:
         saldo -= valorSaque
         print("Valor sacado: ", valorSaque)
         print("Seu saldo atual ficou com um valor de: ", saldo)
@@ -20,7 +31,29 @@ elif resp == 2:
         print("Não é possível depositar ", valorDeposito, "Reais!")
         valorDeposito = int(input("Digite o valor a ser depositado:"))
 
-    else:
-        saldo += valorDeposito
-        print("Valor de ", valorDeposito, "Reais depositado!")
-        print("Seu saldo atual ficou com um valor de: ", saldo)
+   
+        
+    saldo += valorDeposito
+    print("Valor de ", valorDeposito, "Reais depositado!")
+    print("Seu saldo atual ficou com um valor de: ", saldo)
+
+
+       
+elif resp == 3:
+    print("O seu saldo é ", saldo)
+
+
+elif resp == 4:
+    print("Prorama encerrado!")
+
+else:
+    print("Numero inválido, Digite novamente")
+        
+
+    
+
+
+
+
+
+    
